250ca3a7b7abc8f5110142e667ac44132687ae5e
Agustin Szyszczyc <szyagustin@gmail.com>
2022-01-09 22:05:41 -0300

    adding firebase

---
+++ src/services/Firebase.tsx
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyClY0SNVy2GWLcf1usINj-S2e1_BN30lso",
+  authDomain: "szy-ecommerce.firebaseapp.com",
+  projectId: "szy-ecommerce",
+  storageBucket: "szy-ecommerce.appspot.com",
+  messagingSenderId: "286437381058",
+  appId: "1:286437381058:web:ec7b2bb8be222765337c5b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const getFirebase = () => {
+    return app;
+}
+
+export const getFirestoreDB = () => {
+    return getFirestore(app);
+}
